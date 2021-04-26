14cd965af98c066f8582f03d51ba9e23ab40b966
8bitjustin <j.olson.digital@gmail.com>
2021-04-26 14:27:40 -0700

    Created c2j_split.py file. This is to test new method of batching files.

---
+++ c2j_split.py
@@ -0,0 +1,35 @@
+import csv
+import json
+from time import time
+
+start = time()
+
+
+def make_json(csv_path, json_path):
+    data = []
+
+    with open(csv_path, encoding='utf-8-sig') as csvp:
+        csv_reader = csv.DictReader(csvp)
+
+        for row in csv_reader:
+            row_items = list(row.items())
+            for k, v in row_items:
+                if v == "":
+                    del row[k]
+
+            data.append(row)
+
+    output = {
+        "test": data
+    }
+
+    with open(json_path, 'w', encoding='utf-8-sig') as jsonp:
+        jsonp.write(json.dumps(output, indent=2))
+
+
+csv_path = r'elearning Test Records_v2.csv'
+json_path = r'elearning Test Records_v2_indent_test_01.json'
+
+make_json(csv_path, json_path)
+
+print(f'Time taken to run: {time() - start} seconds')
