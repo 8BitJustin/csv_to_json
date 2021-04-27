d9421d1b0975c654b1ed86111347bb74fff0ada7
8bitjustin <j.olson.digital@gmail.com>
2021-04-26 17:17:31 -0700

    Counter test code worked. Altered code to create file for every 10 items in the .csv file.

--- c2j_split.py
+++ c2j_split.py
@@ -11,6 +11,7 @@ def make_json(csv_path):
     counter = 0
 
     with open(csv_path, encoding='utf-8-sig') as csvp:
+
         csv_reader = csv.DictReader(csvp)
 
         for row in csv_reader:
@@ -23,7 +24,11 @@ def make_json(csv_path):
             counter += 1
 
             if counter%10 == 0:
-                print(f'{counter} is multiple of 10')
+                json_path = rf'test{counter}.json'
+                with open(json_path, 'w', encoding='utf-8-sig') as jsonp:
+                    jsonp.write(json.dumps(data, indent=2))
+
+                data = []
 
     print(len(data))
 
@@ -31,12 +36,8 @@ def make_json(csv_path):
         "test": data
     }
 
-    with open(json_path, 'w', encoding='utf-8-sig') as jsonp:
-        jsonp.write(json.dumps(data, indent=2))
-
 
 csv_path = r'random.csv'
-json_path = r'random_batch.json'
 
 make_json(csv_path)
 
